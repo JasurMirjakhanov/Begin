8fce74f1245466926c8264f7f6bd69904a3049ec
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:57:20 +0500

    11-dastur

---
+++ begin_11.py
@@ -0,0 +1,14 @@
+"""Nolga teng bo'lmagan ikkita son berilgan. Ularning yig'indisi, ko'paytmasi va
+har birining moduli aniqlansin"""
+
+x = float(input('1-son: '))
+y = float(input('2- son: '))
+yig = x + y            #yig'indi
+kup = x * y            #ko'paytma
+x2 = abs(x)            #modul [abs]
+y2 = abs(y)
+print("Yig'indisi", yig)
+print("ko'paytmasi", kup)
+print("1 - sonning moduli", x2)
+print("2 - sonning moduli", y2)
+
