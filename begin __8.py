0421f4ab24a82eca8ef3ff7872428683720fb5a2
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:38:46 +0500

    8-dastur

---
+++ begin __8.py
@@ -0,0 +1,6 @@
+"""Ikkita son a va b berilgan. Ularning o'rta arifmetigi aniqlansin."""
+
+a = float (input('a = '))
+b = float (input('b = '))
+c = (a+b)/2              #o'rta arifmetik
+print(" o'rta arifmetigi ->", c)
