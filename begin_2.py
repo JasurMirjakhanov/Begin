49a8903cc1e6e9458735abcdbd68e08bda1df463
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:10:55 +0500

    2-dastur

---
+++ begin_2.py
@@ -0,0 +1,6 @@
+#Kvadratning tomoni a berilgan. Uning yuzasi aniqlansin.
+
+a = float(input('Kvadratning tomonini kiriting: '))
+s=a*a              #1-usul
+"""s=a**2"""       #1-usul
+print('Kvadratning yuzi: ', s)
