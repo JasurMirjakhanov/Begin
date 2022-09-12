c2037fc610f36d34ef0ff49c0981e2fbf8622256
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:20:45 +0500

    5-dastur

---
+++ begin_5.py
@@ -0,0 +1,6 @@
+""" Kubning yon tomoni a berilgan. Uning hajmini va to'la sirti aniqlansin."""
+
+a = float(input('Kubning tomonini kiriting: '))
+v = a ** 3              #hajm
+s = 6 * a * a           #to'la sirti
+print('Hajmi ->',v,"To'la sirti ->",s)
