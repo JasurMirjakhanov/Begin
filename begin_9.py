f873f82e3dab8621e4fbc48e90d948ddf5b40c22
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:41:58 +0500

    9-dastur

---
+++ begin_9.py
@@ -0,0 +1,6 @@
+"""Ikkita manfiy bo'lmagan son a va b berilgan. Ularning o'rta geometrigi aniqlansin."""
+
+a = float (input('a = '))
+b = float (input('b = '))
+c = (a*b)**(1./2)
+print("Orta geometrigi ->", c)
