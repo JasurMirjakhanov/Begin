37618e56cc8f4c604d20b867a1ce067c127e7371
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:48:04 +0500

    10-dastur

---
+++ begin_10.py
@@ -0,0 +1,13 @@
+"""#Nolga teng bo'lmagan ikkita son berilgan. Ularning yig'indisi, ko'paytmasi va
+har birining kvadrati aniqlansin"""
+
+x = float(input('1 - son: '))
+y = float(input('2 - son: '))
+yig = x + y            #yig'indi
+kup = x * y            #ko'paytma
+x2 = x*x
+y2 = y*y
+print("Yig'indisi ->", yig)
+print("ko'paytmasi ->", kup)
+print("Birinchi sonning kvadrati ->", x2)
+print("Ikkinchi sonning kvadrati ->", y2)
