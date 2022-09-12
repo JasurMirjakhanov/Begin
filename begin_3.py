de11b78f457a46ba69d6bb57ff2d31f58b9eb4ce
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:15:02 +0500

    3-dastur

---
+++ begin_3.py
@@ -0,0 +1,7 @@
+# To'g'ri to'rtburchakning tomonlari a va b berilgan. Uning yuzasi va perimetri aniqlansin.
+print("to'g'ri to'rtburchakning tomonlarini kiriting -> ")
+a = float(input('a = '))
+b = float(input('b = '))
+s = a*b       #yuzi
+p = 2*(a+b)   #perimetr
+print('Yuzi ->',s,'Perimetri ->',p)
