060cab777f60b570e74e02d80e1193759c94a039
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-12 22:18:08 +0500

    4-dastur

---
+++ begin_4.py
@@ -0,0 +1,6 @@
+"""Aylananing diametri d berilgan. Uning uzunligini aniqlansin."""
+
+pi = 3.14
+d = float (input('Diametrni kiriting: '))
+L = pi * d              #aylananing uzunligi
+print('Aylana uzunligi ->',L)
