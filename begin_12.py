e63fcdd2730731734c2a7b2a87b99745683984cf
JasurMirjakhanov <jasurmirjaxonov3866@gmail.com>
2022-09-26 10:14:40 +0500

    12-dastur

---
+++ begin_12.py
@@ -0,0 +1,8 @@
+"""To'g'ri uchburchakning katetlari a va b berilgan. Uning gipotenuzasi c  va perimetri p aniqlansin"""
+
+a = int ( input (" a= "))
+b = int ( input (" b= "))
+c=(a**2+b**2)**0.5
+print("c=", c)
+p=a+b+c
+print("p=", p)
